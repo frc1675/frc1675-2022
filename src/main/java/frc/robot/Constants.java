9f3c67183fcd56ef1c6d20c4a4a481d72c17947e
Justin Lanning <justin.lanning@ge.com>
2022-01-17 17:54:02 -0600

    Added cheesy drive and drive subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,13 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final int RIGHT_FRONT_CHANNEL= 0;
+    public static final int RIGHT_BACK_CHANNEL = 1;
+    public static final int LEFT_FRONT_CHANNEL = 2;
+    public static final int LEFT_BACK_CHANNEL = 3; 
+
+    public static final int CONTROLLER_CHANNEL = 0;
+    public static final int LEFT_AXIS_CHANNEL= 1;
+    public static final int RIGHT_AXIS_CHANNEL = 4;
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.CheesyDrive;
+import frc.robot.subsystems.Drive;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,9 +19,10 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Joystick m_driverController = new Joystick(Constants.CONTROLLER_CHANNEL);
+  private final Drive m_drive = new Drive();
+  private final CheesyDrive m_cheesydrive = new CheesyDrive(m_drive, m_driverController);
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -34,7 +36,9 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    m_drive.setDefaultCommand(m_cheesydrive);
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -43,6 +47,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return m_cheesydrive;
   }
 }
