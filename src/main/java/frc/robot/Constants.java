bbc5da2914f64960e8ca4c328e6ef4dd316ff83d
Golden Experience <ups1675@gmail.com>
2022-02-01 18:39:06 -0600

    Added complete controller button map

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -38,15 +38,24 @@ public final class Constants {
     //controller constants
     public static final int DRIVER_CONTROLLER = 0;
     public static final int OPERATOR_CONTROLLER = 1;
+
     public static final int LEFT_X_AXIS = 0;
     public static final int LEFT_Y_AXIS= 1;
     public static final int RIGHT_X_AXIS = 4;
     public static final int RIGHT_Y_AXIS = 5;
+
     public static final int A_BUTTON = 1;
     public static final int B_BUTTON = 2;
     public static final int X_BUTTON = 3;
-    public static final int START_BUTTON = 11;
+    public static final int Y_BUTTON = 4;
+
+    public static final int LEFT_BUMPER = 5;
     public static final int RIGHT_BUMPER = 6;
+    public static final int BACK_BUTTON = 7;
+    public static final int START_BUTTON = 8;
+    public static final int LEFT_JOYSTICK_BUTTON = 9;
+    public static final int RIGHT_JOYSTICK_BUTTON = 10;
+    
 
     //mathematical constants
     public static final double DEADZONE_CONSTANT = 0.1675;
