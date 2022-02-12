7a7c396d8fe852619fe6deed3e7855d2b42019ae
MPS Gray <UPS1675@gmail.com>
2022-02-12 14:31:59 -0600

    Intake speed controls now accept joystick input... ...via DoubleSuppliers.

--- src/main/java/frc/robot/commands/intake/SetIntakeSpeed.java
+++ src/main/java/frc/robot/commands/intake/SetIntakeSpeed.java
@@ -3,14 +3,16 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands.intake;
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Intake;
 
 public class SetIntakeSpeed extends CommandBase {
   private final Intake intake;
-  private final double speed;
+  private final DoubleSupplier speed;
 
-  public SetIntakeSpeed(Intake intake, double speed) {
+  public SetIntakeSpeed(Intake intake, DoubleSupplier speed) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.intake = intake;
     this.speed= speed;
@@ -20,12 +22,14 @@ public class SetIntakeSpeed extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    intake.setIntakeSpeed(speed);
+    //intake.setIntakeSpeed(speed);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    intake.setIntakeSpeed(speed.getAsDouble());
+  }
 
   // Called once the command ends or is interrupted.
   @Override
