6c5b1d0478d58e4dc3dd7fc4ef27cf408895b926
Strange Journey <ups1675@gmail.com>
2022-02-14 19:32:51 -0600

    Tenative area4getball3 command

--- src/main/java/frc/robot/commands/auto/Area4GetBall3.java
+++ src/main/java/frc/robot/commands/auto/Area4GetBall3.java
@@ -24,15 +24,19 @@ public class Area4GetBall3 extends SequentialCommandGroup {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
+      new TurnToAngle(drive, 37, 1),
       new ExtendIntake(intake),
       new ParallelDeadlineGroup(
-        new DriveToDistance(drive, 50, 1).withTimeout(3),
+        new DriveToDistance(drive, 48, 1),
         new SetIntakeSpeed(intake, 1)
       ),
       new RetractIntakeSafe(intake, cage, catapult),
-      new TurnToAngle(drive, 180, 1).withTimeout(2),
-      new DriveToDistance(drive, 100, 1).withTimeout(3),
-      new FireAnyCatapultsSafe(intake, cage, catapult, true, true)
+      new TurnToAngle(drive, -44, 1),
+      new DriveToDistance(drive, -48, 1),
+      new TurnToAngle(drive, 27, 1),
+      new DriveToDistance(drive, -31.5, 1),
+      new FireAnyCatapultsSafe(intake, cage, catapult, true, true),
+      new DriveToDistance(drive, 63, 1)
     );
   }
 }
