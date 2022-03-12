639bd8d834b0ad2f7c13903583d8f521d5c0e5e0
Lanning <lanninjd@milwaukee.k12.wi.us>
2022-03-12 10:35:34 -0600

    vision class code review changes

--- src/main/java/frc/robot/utils/Vision.java
+++ src/main/java/frc/robot/utils/Vision.java
@@ -3,8 +3,9 @@ package frc.robot.utils;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Vision {
-    public double isTargetValid(){
-        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+    public boolean isTargetValid(){
+        //convert this to a boolean using if statements or anything else you can think of
+        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0) == 1;
     }
     public double horizOffset(){
         return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
@@ -14,5 +15,14 @@ public class Vision {
     }
     public double areaPercent(){
         return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+    }
+
+    public void toggleCamMode(){
+        if(NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").getDouble(0)==0){
+            NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(1);
+        }
+        else{
+            NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(0);
+        }
     }
 }
