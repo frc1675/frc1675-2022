24d1426bc870f076dcdae76d1c1995fd1133196d
Strange Journey <ups1675@gmail.com>
2022-03-22 19:25:47 -0500

    Three ball auto tested and working consistently

--- src/main/java/frc/robot/commands/auto/DriveToDistance.java
+++ src/main/java/frc/robot/commands/auto/DriveToDistance.java
@@ -38,6 +38,7 @@ public class DriveToDistance extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    super.end(interrupted);
     drive.setRight(0);
     drive.setLeft(0);
   }
