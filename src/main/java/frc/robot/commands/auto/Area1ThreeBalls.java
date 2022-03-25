6b580f4080fbed729c9ad70e70f43507ac63cf9e
Strange Journey <ups1675@gmail.com>
2022-03-25 16:07:03 -0500

    Changed 3 ball auto timings

--- src/main/java/frc/robot/commands/auto/Area1ThreeBalls.java
+++ src/main/java/frc/robot/commands/auto/Area1ThreeBalls.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands.auto;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -42,14 +43,14 @@ public class Area1ThreeBalls extends SequentialCommandGroup {
       ),
       new ExtendThenRunIntake(intake, cage, rightCatapult, leftCatapult, () -> {return Constants.INTAKE_CONSTANT_SPEED;})
       .withTimeout(0.5),
-      new ParallelDeadlineGroup(
+      new ParallelCommandGroup(
         new SequentialCommandGroup(
           new TurnToAngleWithTimeout(drive, -62, 0.5),
-          new DriveToDistanceWithTimeout(drive, -60, 0.5)
+          new DriveToDistanceWithTimeout(drive, -48, 0.5)
         ),
         new SequentialCommandGroup(
           new RetractIntakeSafe(intake, cage, rightCatapult, leftCatapult),
-          new WaitCommand(0.7),
+          new WaitCommand(1.5),
           new PrepareCatapultFire(intake, cage)
         )
       ),
